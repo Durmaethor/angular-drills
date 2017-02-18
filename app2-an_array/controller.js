f0665fe6820b57c36eb137e845059e65276ce326
Ryan Kramer <rkramer613@gmail.com>
2017-02-18 12:11:33 -0700

    ctrl and srv setup

--- app2-an_array/controller.js
+++ app2-an_array/controller.js
@@ -1,7 +1,11 @@
 angular.module('arrayApp').controller('arrayController', function($scope){
 
+    $scope.getArray = function(){
+        $scope.array = arrayService.getArray();
+    }
+
+    $scope.getArray();
 
 
 
-    
 })
